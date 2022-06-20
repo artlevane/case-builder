6e2635d6c1c715a3dea26ea838c9b511aa32d754
Artemis Levane <art@artlevane.com>
2022-06-19 22:36:58 -0400

    foundation done. next is adding troubleshootings steps.

--- index.php
+++ index.php
@@ -11,8 +11,15 @@
 
                   <!-- CASE SYNOPSIS-->
                   <label for="synopsis">Reason for calling: &nbsp;</label>
-                  <input type="text" id="synopsis" name="synopsis"><br><br>
+                  <input type="text" id="synopsis" name="synopsis"> &nbsp;
                   <!-- CASE SYNOPSIS END -->
+
+                  <!-- ERROR MESSAGES -->
+                  <label for="err_messages">Error messages: &nbsp;</label>
+                  <input type="text" id="err_messages" name="err_messages">
+                  <!-- ERROR MESSAGES -->
+
+                  <br><br>
 
                   <!-- SCANNER DROPDOWN -->
                   <label for="Scanners">Choose a scanner: &nbsp;</label>
@@ -51,60 +58,72 @@
                     </select>
                     <!-- SCANNER DROPDOWN END -->
 
-                    <!-- OS DROPDOWN -->
-                    <label for="OS">&nbsp; Operating System (OS): &nbsp;</label>
-                    <select name="OS" id="OS">
+                  <!-- OS DROPDOWN -->
+                  <label for="OS">&nbsp; Operating System (OS): &nbsp;</label>
+                  <select name="OS" id="OS">
+                    <option value="None">None</option>
+                    <optgroup label="Windows">
+                        <option value="Win11">Win11</option>
+                        <option value="Win10">Win10</option>
+                        <option value="Win8">Win8</option>
+                        <option value="Win7">Win7</option>
+                    </optgroup>
+                    <optgroup label="Mac">
+                        <option value="Monterey">Monterey</option>
+                        <option value="Big Sur">Big Sur</option>
+                        <option value="Catalina">Catalina</option>
+                        <option value="Mojave">Mojave</option>
+                    </optgroup>
+                  </select>
+                  <!-- OS DROPDOWN END -->
+
+                  <!-- SOFTWARE DROPDOWN -->
+                  <label for="Software">&nbsp; ScanSnap Software: &nbsp;</label>
+                  <select name="software" id="software">
                       <option value="None">None</option>
-                      <optgroup label="Windows">
-                          <option value="Win11">Win11</option>
-                          <option value="Win10">Win10</option>
-                          <option value="Win8">Win8</option>
-                          <option value="Win7">Win7</option>
+                      <optgroup label="ScanSnap Home">
+                          <option value="SSH">SSH</option>
+                          <option value="SSHv2.6.0">SSHv2.6.0</option>
                       </optgroup>
-                      <optgroup label="Mac">
-                          <option value="Monterey">Monterey</option>
-                          <option value="Big Sur">Big Sur</option>
-                          <option value="Catalina">Catalina</option>
-                          <option value="Mojave">Mojave</option>
+                      <optgroup label="ScanSnap Manager">
+                          <option value="SSM">SSM</option>
+                          <option value="SSMv7.2">SSMv7.2</option>
+                          <option value="SSMv6.5">SSMv6.5</option>
+                          <option value="SSMv6">SSMv6</option>
+                          <option value="SSMv5.5">SSMv5.5</option>
+                          <option value="SSMv4.5">SSMv4.5</option>
                       </optgroup>
-                    </select>
-                    <!-- OS DROPDOWN END -->
+                  </select>
+                  <!-- SOFTWARE DROPDOWN END -->
 
-                      <!-- SOFTWARE DROPDOWN -->
-                      <label for="Software">&nbsp; ScanSnap Software: &nbsp;</label>
-                      <select name="software" id="software">
-                          <option value="None">None</option>
-                          <optgroup label="ScanSnap Home">
-                              <option value="SSH">SSH</option>
-                              <option value="SSHv2.6.0">SSHv2.6.0</option>
-                          </optgroup>
-                          <optgroup label="ScanSnap Manager">
-                              <option value="SSM">SSM</option>
-                              <option value="SSMv7.2">SSMv7.2</option>
-                              <option value="SSMv6.5">SSMv6.5</option>
-                              <option value="SSMv6">SSMv6</option>
-                              <option value="SSMv5.5">SSMv5.5</option>
-                              <option value="SSMv4.5">SSMv4.5</option>
-                          </optgroup>
-                      </select>
-                      <!-- SOFTWARE DROPDOWN END -->
-
-                      <br><br>
+                  <br><br>
 
                   <!-- CONNECTIVITY -->
-                    <label for="connection-type">&nbsp; Connection Type: &nbsp;</label>
-                    <select name="connection-type" id="connection-type">
-                      <option value="None">None</option>
-                      <optgroup label="Wireless">Wireless</optgroup>
-                      <option value="Wirelessly connected">Wireless</option>
-                      <optgroup label="USB">USB</optgroup>
-                      <option value="Plugged directly into PC via USB">USB directly into PC</option>
-                      <option value="Plugged into hub or dock via USB">USB via hub or dock</option>
-                      <option value="Plugged into adapter via USB">USB via adapter</option>
-                    </select>
-                    <!-- CONNECTIVITY END -->
-                    <br><br>
-                    <input type="submit" value="Submit">
+                  <label for="connection-type">&nbsp; Connection Type: &nbsp;</label>
+                  <select name="connection-type" id="connection-type">
+                    <option value="None">None</option>
+                    <optgroup label="Wireless">Wireless</optgroup>
+                    <option value="Wirelessly connected.">Wireless</option>
+                    <optgroup label="USB">USB</optgroup>
+                    <option value="Plugged directly into PC via USB.">USB directly into PC</option>
+                    <option value="Plugged into hub or dock via USB.">USB via hub or dock</option>
+                    <option value="Plugged into adapter via USB.">USB via adapter</option>
+                  </select>
+                  <!-- CONNECTIVITY END -->
+
+                  <br><br>
+
+                  <!-- REMOTE IN -->
+                  <label for="remote_in">Remote-in:&nbsp;</label>
+                  <input type="checkbox" name="remote_in" id="remote_in">
+                  <!-- REMOTE IN END -->
+
+                  <br><br>
+
+
+                  <!-- SUBMIT BUTTON -->
+                  <input type="submit" value="Submit">
+                  <!-- SUBMIT BUTTON END -->
               </form>
               <!-- FORM ENDS -->
 
@@ -113,10 +132,12 @@
                 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     $case_synopsis = $_POST["synopsis"];
+                    $err_messages = $_POST["err_messages"];
                     $scanner_model = $_POST["scanners"];
                     $OS = $_POST["OS"];
                     $software = $_POST["software"];
                     $connection_type = $_POST["connection-type"];
+                    $remote_in = $_POST["remote_in"];
 
                     if ($scanner_model == "None") {
                       $scanner_model = "";
@@ -138,7 +159,21 @@
                       $connection_type = "";
                     }
 
+                    if ($remote_in == TRUE) {
+                      $remote_in = "<br>Remoted in.<br>-";
+                    } else {
+                      $remote_in = "<br>Rejected remote in.<br>-";
+                    }
+
+                    if ($err_messages == "") {
+                      $err_messages = "";
+                    } else {
+                      $err_messages = "<br>" . $err_messages;
+                    }
+
                     echo $case_synopsis;
+                    echo $err_messages;
+                    echo $remote_in;
                     echo "<br>";
                     echo $scanner_model;
                     echo $OS;
